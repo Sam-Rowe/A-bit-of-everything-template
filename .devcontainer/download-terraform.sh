89bb05dd8f2294096b03bf0225dd61f4ab1d19ff
Samuel Rowe <14168597+Sam-Rowe@users.noreply.github.com>
2021-02-07 15:44:25 +0000

    Terraform install moved to script in post create command

---
+++ .devcontainer/download-terraform.sh
@@ -0,0 +1,5 @@
+cd ~/
+wget https://releases.hashicorp.com/terraform/0.14.6/terraform_0.14.6_linux_amd64.zip
+unzip terraform_0.14.6_linux_amd64.zip
+mkdir /root/.local/bin/
+cp terraform .local/bin/terraform
